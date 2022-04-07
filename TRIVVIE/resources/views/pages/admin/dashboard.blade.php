03ebb91668ba06539b2ea73fad93c93e16971679
NurulH28 <nurulh2810@gmail.com>
2022-04-07 16:25:36 +0700

    Add: layout 2 | Admin Page ref #5

--- TRIVVIE/resources/views/pages/admin/dashboard.blade.php
+++ TRIVVIE/resources/views/pages/admin/dashboard.blade.php
@@ -8,6 +8,8 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+        class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
      </div>
 
     <!-- Content Row -->
@@ -105,9 +107,9 @@
                             aria-labelledby="dropdownMenuLink">
                             <div class="dropdown-header">Dropdown Header:</div>
                             <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
+                            <!-- <a class="dropdown-item" href="#">Another action</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" href="#">Something else here</a> -->
                         </div>
                     </div>
                 </div>
@@ -136,9 +138,9 @@
                             aria-labelledby="dropdownMenuLink">
                             <div class="dropdown-header">Dropdown Header:</div>
                             <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
+                            <!-- <a class="dropdown-item" href="#">Another action</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" href="#">Something else here</a> -->
                         </div>
                     </div>
                 </div>
@@ -149,7 +151,7 @@
                     </div>
                     <div class="mt-4 text-center small">
                         <span class="mr-2">
-                            <i class="fas fa-circle text-primary"></i> Berhasil
+                            <i class="fas fa-circle text-primary"></i> Sukses
                         </span>
                         <span class="mr-2">
                             <i class="fas fa-circle text-success"></i> Pending
