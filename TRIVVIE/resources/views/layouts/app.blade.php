ab7f18735842dffa0d23d3e82cb3510845a3113c
Iqbal Fadillah <Muhammad.Iqbal.Fadillah@hashmicro.com>
2022-04-18 14:17:24 +0700

    templating landing page

--- TRIVVIE/resources/views/layouts/app.blade.php
+++ TRIVVIE/resources/views/layouts/app.blade.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Landing Page</title>
-    <link rel="stylesheet" href="frontend/libraries/bootstrap/css/bootstrap.css" />
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{url('frontend/libraries/bootstrap/css/bootstrap.css')}}" />
     <link
         href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600,700,800|Playfair+Display:400,400i,700,700i,900,900i&display=swap"
         rel="stylesheet" />
-    <link rel="stylesheet" href="frontend/styles/main.css" />
+    <link rel="stylesheet" href="{{url('frontend/styles/main.css')}}" />
 </head>
 
 <body>
@@ -63,202 +63,7 @@
         </nav>
     </div>
 
-    <header class="text-center">
-        <h1>
-            Explore The Beautiful World
-            <br />
-            As Easy One Click
-        </h1>
-        <p class="mt-3">
-            You will see beautiful
-            <br />
-            moment you never see before
-        </p>
-        <a href="#" class="btn btn-get-started px-4 mt-4">
-            Get Started
-        </a>
-    </header>
-
-    <main>
-        <div class="container">
-            <section class="section-stats row justify-content-center" id="stats">
-                <div class="col-3 col-md-2 stats-detail text-center">
-                    <h2>20K</h2>
-                    <p>Members</p>
-                </div>
-                <div class="col-3 col-md-2 stats-detail text-center">
-                    <h2>12</h2>
-                    <p>Countries</p>
-                </div>
-                <div class="col-3 col-md-2 stats-detail text-center">
-                    <h2>3K</h2>
-                    <p>Hotels</p>
-                </div>
-                <div class="col-3 col-md-2 stats-detail text-center">
-                    <h2>72</h2>
-                    <p>Partners</p>
-                </div>
-            </section>
-        </div>
-
-        <section class="section-popular" id="popular">
-            <div class="container">
-                <div class="row">
-                    <div class="col text-center section-popular-heading">
-                        <h2>Wisata Popular</h2>
-                        <p>
-                            Something that you never try
-                            <br />
-                            before in this world
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="section-popular-content" id="popularContent">
-            <div class="container">
-                <div class="section-popular-travel row justify-content-center">
-                    <div class="col-sm-6 col-md-4 col-lg-3">
-                        <div class="card-travel text-center d-flex flex-column"
-                            style="background-image: url('frontend/images/tr1.jpg');">
-                            <div class="travel-country">INDONESIA</div>
-                            <div class="travel-location">UBUD, BALI</div>
-                            <div class="travel-button mt-auto">
-                                <a href="#" class="btn btn-travel-details px-4">
-                                    View Details
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4 col-lg-3">
-                        <div class="card-travel text-center d-flex flex-column"
-                            style="background-image: url('frontend/images/tr2.jpg');">
-                            <div class="travel-country">JAPAN</div>
-                            <div class="travel-location">OSAKA</div>
-                            <div class="travel-button mt-auto">
-                                <a href="#" class="btn btn-travel-details px-4">
-                                    View Details
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4 col-lg-3">
-                        <div class="card-travel text-center d-flex flex-column"
-                            style="background-image: url('frontend/images/tr3.jpg');">
-                            <div class="travel-country">INDONESIA</div>
-                            <div class="travel-location">MANDALIKA</div>
-                            <div class="travel-button mt-auto">
-                                <a href="#" class="btn btn-travel-details px-4">
-                                    View Details
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4 col-lg-3">
-                        <div class="card-travel text-center d-flex flex-column"
-                            style="background-image: url('frontend/images/tr4.jpg');">
-                            <div class="travel-country">EUROPE</div>
-                            <div class="travel-location">FRENCH</div>
-                            <div class="travel-button mt-auto">
-                                <a href="#" class="btn btn-travel-details px-4">
-                                    View Details
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="section-networks" id="networks">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        <h2>Our Networks</h2>
-                        <p>
-                            Companies are trusted us
-                            <br />
-                            more than just a trip
-                        </p>
-                    </div>
-                    <div class="col-md-8 text-center">
-                        <img src="frontend/images/partner.png" alt="Logo Partner" class="img-partner" />
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="section-testimonial-heading" id="testimonialHeading">
-            <div class="container">
-                <div class="row">
-                    <div class="col text-center">
-                        <h2>They Are Loving Us</h2>
-                        <p>
-                            Moments were giving them
-                            <br />
-                            the best experience
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="section-testimonial-content" id="testimonialContent">
-            <div class="container">
-                <div class="section-popular-travel row justify-content-center match-height">
-                    <div class="col-sm-6 col-md-6 col-lg-4">
-                        <div class="card card-testimonial text-center">
-                            <div class="testimonial-content">
-                                <h3 class="mb-4">John Doe</h3>
-                                <p class="testimonial">
-                                    “ It was glorious and I could not stop to say wohooo for
-                                    every single moment Dankeeeeee “
-                                </p>
-                            </div>
-                            <hr />
-                            <p class="trip-to mt-2">Trip to Ubud</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-6 col-lg-4">
-                        <div class="card card-testimonial text-center">
-                            <div class="testimonial-content">
-                                <h3 class="mb-4">Grace anne</h3>
-                                <p class="testimonial">
-                                    “ The trip was amazing and I saw something beautiful in that
-                                    Island that makes me want to learn more “
-                                </p>
-                            </div>
-                            <hr />
-                            <p class="trip-to mt-2">Trip to Nusa Penida</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-6 col-lg-4">
-                        <div class="card card-testimonial text-center">
-                            <div class="testimonial-content mb-auto">
-                                <h3 class="mb-4">Jane Wine</h3>
-                                <p class="testimonial">
-                                    “ I loved it when the waves was shaking harder — I was
-                                    scared too “
-                                </p>
-                            </div>
-                            <hr />
-                            <p class="trip-to mt-2">Trip to Karimun Jawa</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 text-center">
-                        <a href="#" class="btn btn-need-help px-4 mt-4 mx-1">
-                            I Need Help
-                        </a>
-                        <a href="#" class="btn btn-get-started px-4 mt-4 mx-1">
-                            Get Started
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </main>
+    @yield('content')
 
     <footer class="section-footer mt-5 mb-4 border-top">
         <div class="container pt-5 pb-5">
@@ -323,9 +128,9 @@
             </div>
         </div>
     </footer>
-    <script src="frontend/libraries/retina/retina.min.js"></script>
-    <script src="frontend/libraries/jquery/jquery-3.6.0.min.js"></script>
-    <script src="frontend/libraries/bootstrap/js/bootstrap.js"></script>
+    <script src="{{url('frontend/libraries/retina/retina.min.js')}}"></script>
+    <script src="{{url('frontend/libraries/jquery/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{url('frontend/libraries/bootstrap/js/bootstrap.js')}}"></script>
 </body>
 
 </html>
--- TRIVVIE/routes/web.php
+++ TRIVVIE/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\TravelPackageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\CheckoutController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +19,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Route::prefix('admin')
     ->namespace('Admin')
-    ->group(function() {
+    ->group(function () {
         Route::get('/', 'DashboardController@index')
             ->name('dashboard');
     });
