ee3ae4e49aa6c7593c7755bc239bf58b1fe2689b
NurulH28 <nurulh2810@gmail.com>
2022-04-24 23:46:13 +0700

    change laravel v.9 to v.8

--- TRIVVIE/app/Providers/RouteServiceProvider.php
+++ TRIVVIE/app/Providers/RouteServiceProvider.php
@@ -19,14 +19,14 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
-        /**
+    /**
      * The controller namespace for the application.
      *
      * When present, controller route declarations will automatically be prefixed with this namespace.
      *
      * @var string|null
      */
-    protected $namespace = 'App\Http\Controllers';
+        protected $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -57,7 +57,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
     }
 }
