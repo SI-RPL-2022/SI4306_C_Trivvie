ac6fc78b91846e9b9b36f520e4944fa1fa0bd99a
NurulH28 <nurulh2810@gmail.com>
2022-04-25 00:24:54 +0700

    change laravel ver

--- #trivvie/resources/views/includes/admin/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Footer -->
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Copyright &copy; TRIVVIE 187C | 2022</span>
-        </div>
-    </div>
-</footer>
-<!-- End of Footer -->
--- #trivvie/resources/views/includes/script.blade.php
+++
@@ -1,3 +0,0 @@
-<script src="{{url('frontend/libraries/retina/retina.min.js')}}"></script>
-<script src="{{url('frontend/libraries/jquery/jquery-3.6.0.min.js')}}"></script>
-<script src="{{url('frontend/libraries/bootstrap/js/bootstrap.js')}}"></script>
