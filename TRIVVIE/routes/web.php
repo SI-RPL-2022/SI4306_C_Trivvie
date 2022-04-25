1c7887dfbb46a582ed9449d4878666039b096a61
Iqbal Fadillah <Muhammad.Iqbal.Fadillah@hashmicro.com>
2022-04-25 08:52:17 +0700

    fix auth

--- TRIVVIE/routes/web.php
+++ TRIVVIE/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\TravelPackageController;
 use App\Http\Controllers\HomeController;
